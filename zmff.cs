f6afc7c48a18f2b5839fa6591974482bd190fe15
ilovedc10 <ilovedc10@naver.com>
2026-06-17 21:28:29 +0900

    UML Map

---
+++ zmff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+
+namespace Assembly_CSharp
+{
+    [RunInstaller(true)]
+    public partial class zmff : System.Configuration.Install.Installer
+    {
+        public zmff()
+        {
+            InitializeComponent();
+        }
+    }
+}
