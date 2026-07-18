a644c7fbb95183246dcc2f9bb6f2f42788a1f74a
jihoryu <jihoryu8@gmail.com>
2026-07-18 11:39:36 +0900

    아트를 가장 최신판에도 적용하였습니다

--- Assets/scripts/Cards/CardVisual.cs
+++ Assets/scripts/Cards/CardVisual.cs
@@ -23,7 +23,7 @@ public class CardVisual : MonoBehaviour
         _background      = transform.Find("Front/background").GetComponent<SpriteRenderer>();
         _sprite          = transform.Find("Front/sprite").GetComponent<SpriteRenderer>();
         _effectText      = transform.Find("Front/effect/effectText").GetComponent<TextMeshPro>();
-        _costText        = transform.Find("Front/CostText").GetComponent<TextMeshPro>();
+        _costText        = transform.Find("Front/cost/CostText").GetComponent<TextMeshPro>();
         _cooltimeText    = transform.Find("Front/cooltimeText").GetComponent<TextMeshPro>();
         _selectHighlight = transform.Find("Front/SelectHighlight").gameObject;
         _selectHighlight.SetActive(false);
